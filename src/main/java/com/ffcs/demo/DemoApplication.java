a566176bcefccabf4f22ab0682a7693bbe823bf6
2020xiaohe <1766282543@qq.com>
2020-08-15 23:56:37 +0800

    打包配置与请求图片接口

--- src/main/java/com/ffcs/demo/DemoApplication.java
+++ src/main/java/com/ffcs/demo/DemoApplication.java
@@ -3,12 +3,20 @@ package com.ffcs.demo;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @MapperScan("com.ffcs.demo.dao.mapper")
 @EnableSwagger2
 public class DemoApplication {
+
+//	// 打包
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(DemoApplication.class);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
